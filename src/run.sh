9a47c0fc85855843cde9116bda8ac0e1f879c7eb
Vicente Cunha <vicentenmc@gmail.com>
2023-12-05 19:52:06 +0000

    fiz o run mais rapido

--- src/run.sh
+++ src/run.sh
@@ -13,13 +13,13 @@ make clean
 make
 
 gnome-terminal  --full-screen -- bash -c "./motor; exit"
-sleep 2
+sleep 0.5
 gnome-terminal  --full-screen -- bash -c "./jogoUI nome1; exit"
-sleep 1
+sleep 0.1
 gnome-terminal  --full-screen -- bash -c "./jogoUI nome2; exit"
-sleep 1
+sleep 0.1
 gnome-terminal  --full-screen -- bash -c "./jogoUI nome3; exit"
-sleep 1
+sleep 0.1
 gnome-terminal  --full-screen -- bash -c "./jogoUI nome4; exit"
-sleep 1
+sleep 0.1
 gnome-terminal  --full-screen -- bash -c "./jogoUI nome5; exit"
